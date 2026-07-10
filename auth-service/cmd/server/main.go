68af01e939ae26a2f031e617eb4d96163c149463
Михаил <mishakhegai@gmail.com>
2026-07-10 17:26:11 +0300

    feat(auth-service): add base server and config.go

---
+++ auth-service/cmd/server/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"bookshelf/auth-service/internal/config"
+)
+
+func main() {
+	// config
+	cfg := config.Load()
+
+	// routing
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	// health handler
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		resp := map[string]string{
+			"status": "ok",
+		}
+
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			log.Println("failed to encode response:", err)
+		}
+	})
+
+	// graceful shutdown
+	srv := &http.Server{
+		Addr:         ":" + cfg.Server.Port,
+		Handler:      r,
+		ReadTimeout:  cfg.Server.ReadTimeout,
+		WriteTimeout: cfg.Server.WriteTimeout,
+		IdleTimeout:  cfg.Server.IdleTimeout,
+	}
+
+	go func() {
+		log.Printf("Server started on %s", srv.Addr)
+
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("server failed: %v", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("server shutdown failed: %v", err)
+	}
+
+	log.Println("Server stopped")
+}
---
+++ auth-service/go.mod
@@ -0,0 +1,9 @@
+module bookshelf/auth-service
+
+go 1.26.2
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.3.1 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
