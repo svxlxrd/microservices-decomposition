fe479d6e1ae390818d08db62acfa7e6b65b71bce
Михаил <mishakhegai@gmail.com>
2026-07-12 15:31:24 +0300

    feat(config): add config package

---
+++ books-service/internal/config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"os"
+	"time"
+)
+
+type Config struct {
+	Server     ServerConfig
+	Database DatabaseConfig
+	JWT      JWTConfig
+}
+
+type ServerConfig struct {
+	Port         string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+	IdleTimeout  time.Duration
+}
+
+type DatabaseConfig struct {
+	URL string
+}
+
+type JWTConfig struct {
+	Secret string
+}
+
+func Load() *Config {
+	return &Config{
+		Server: ServerConfig{
+			Port:         getEnv("PORT", "8082"),
+			ReadTimeout:  getDuration("READ_TIMEOUT", 5*time.Second),
+			WriteTimeout: getDuration("WRITE_TIMEOUT", 10*time.Second),
+			IdleTimeout:  getDuration("IDLE_TIMEOUT", 60*time.Second),
+		},
+		Database: DatabaseConfig{
+			URL: getEnv(
+				"DATABASE_URL",
+				"postgres://postgres:postgres@localhost:5432/auth?sslmode=disable",
+			),
+		},
+		JWT: JWTConfig{
+			Secret: getEnv("JWT_SECRET", "mock"),
+		},
+	}
+}
+
+func getEnv(key, def string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return def
+}
+
+func getDuration(key string, def time.Duration) time.Duration {
+	if v := os.Getenv(key); v != "" {
+		d, err := time.ParseDuration(v)
+		if err == nil {
+			return d
+		}
+	}
+	return def
+}
+
